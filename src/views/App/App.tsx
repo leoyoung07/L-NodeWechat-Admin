0c932d82b5a59417aede514e85a7bc70795c2b05
Leo Young <420760135@qq.com>
2018-03-18 23:35:01 +0800

    style: setup App layout

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,10 +1,11 @@
-import { Layout } from 'antd';
+import { Breadcrumb, Icon, Layout, Menu } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { IAction, IState } from '../../interfaces';
 import './App.scss';
 const { Header, Footer, Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
 
 interface IProps {
   loggedIn: boolean;
@@ -18,17 +19,63 @@ class App extends React.Component<IProps> {
   render() {
     return (
       <div className="App">
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible={true}
+          // collapsed={this.state.collapsed}
+          // onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Layout>
-          <Header>
-            Header
-            <Link to="/login">logout</Link>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <div className="right">
+              <Link to="/login">logout</Link>
+            </div>
           </Header>
-          <Layout>
-            <Sider>Sider</Sider>
-            <Content>Content</Content>
-          </Layout>
-          <Footer>Footer</Footer>
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              Content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Footer
+          </Footer>
         </Layout>
+      </Layout>
       </div>
     );
   }
