860ca5ed7bb454e217fda1b8be93cf0a02ed1c62
Leo Young <420760135@qq.com>
2018-03-22 20:39:56 +0800

    style: menu designer add modal

--- src/views/MenuDesigner/MenuDesigner.tsx
+++ src/views/MenuDesigner/MenuDesigner.tsx
@@ -1,31 +1,65 @@
-import { Button, List, Popover } from 'antd';
+import { Button, List, Modal, Popover } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
-import { IState } from '../../interfaces';
 import './MenuDesigner.scss';
 
 const ButtonGroup = Button.Group;
-
-const subMenus = (
-  <List
-    dataSource={[1, 2, 3]}
-    renderItem={(item: string) => (
-      <List.Item>{'SubMenu ' + item}</List.Item>
-    )}
-  />
-);
 
 interface IProps {
   user: {};
 }
 
-class MenuDesigner extends React.Component<IProps> {
+interface IState {
+  modalVisible: boolean;
+  currentEditingItem: string | null;
+}
+
+class MenuDesigner extends React.Component<IProps, IState> {
 
   constructor (props: IProps) {
     super(props);
+    this.state = {
+      modalVisible: false,
+      currentEditingItem: null
+    };
+    this.handleModalCancel = this.handleModalCancel.bind(this);
+    this.handleModalOK = this.handleModalOK.bind(this);
+    this.handleSubMenuClick = this.handleSubMenuClick.bind(this);
   }
 
+  handleSubMenuClick (item: string) {
+    this.setState({
+      modalVisible: true,
+      currentEditingItem: item
+    });
+  }
+
+  handleModalOK () {
+    this.setState({
+      modalVisible: false,
+      currentEditingItem: null
+    });
+  }
+
+  handleModalCancel () {
+    this.setState({
+      modalVisible: false,
+      currentEditingItem: null
+    });
+  }
   render() {
+    const subMenus = (
+      <List
+        dataSource={[1, 2, 3]}
+        renderItem={(item: string) => (
+          <List.Item>
+            <div className="menu-designer__sub-menu" onClick={() => { this.handleSubMenuClick(item); }}>
+              {'SubMenu ' + item}
+            </div>
+          </List.Item>
+        )}
+      />
+    );
     return (
       <div className="menu-designer__wrapper">
         <ButtonGroup>
@@ -40,12 +74,21 @@ class MenuDesigner extends React.Component<IProps> {
             </Popover>
           ))}
         </ButtonGroup>
+        <Modal
+          visible={this.state.modalVisible}
+          onOk={this.handleModalOK}
+          onCancel={this.handleModalCancel}
+        >
+          <div>
+            {this.state.currentEditingItem}
+          </div>
+        </Modal>
       </div>
     );
   }
 }
 
-function mapStateToProps(state: IState) {
+function mapStateToProps(state: {}) {
   return {
     user: {}
   };
